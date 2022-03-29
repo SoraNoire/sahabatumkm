982afc7994c373d4db52c72f10e8f4a82c905960
Dicky Aryawan <aryawandicky@gmail.com>
2022-03-29 10:55:47 +0700

    sumkm first

---
+++ pages(prep)/member/home.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SUMKM</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+<style>
+body {font-family: "Raleway", Arial, sans-serif}
+.w3-row img {margin-bottom: -8px}
+</style>
+</head>
+<body>
+
+<div class="w3-content" style="max-width:1500px">
+
+  <!-- Header -->
+  <header class="w3-container w3-xlarge w3-padding-24">
+    <a href="#" class="w3-left w3-button w3-white">CATALOG</a>
+    <a href="#" class="w3-left w3-button w3-white">GALLERY</a>
+    <a href="#" class="w3-left w3-button w3-white">EVENTS</a>
+    <a href="#" class="w3-left w3-button w3-white">(LOGO SUMKM)</a>
+    <a href="#" class="w3-left w3-button w3-white">ABOUT US</a>
+    <a href="#" class="w3-left w3-button w3-white">NEWS</a>
+    <a href="#" class="w3-left w3-button w3-white">CONTACT</a>
+    <a href="#" class="w3-right w3-button w3-white">LOGIN/REGISTER</a>
+  </header>
+
+   <!-- banner -->
+  <div class="w3-container w3-xlarge w3-padding-64">
+        <?php include 'banner.php';?>
+  </div>
+  
+
+    <!-- About -->
+  <div class="w3-container">
+    <table style="width: 100%;">
+        <tr>
+          <td style="width: 7%;">
+            <img src="img/7.jpg">
+          </td>
+          <td>
+            <h2>ABOUT</h2>
+            <h2><B>SAHABAT UMKM</B></h2>
+          </td>
+          <td rowspan="2">
+          <img src="img/123.jpeg" style="max-width:500px">
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2" style="padding: 20px;">
+              Sahabat UMKM merupakan komunitas yang mempertemukan antara Pelaku UMKM dan Profesional Kewirausahaan. Dengan prinsip komunikasi positif dan saling membangun, 
+              Sahabat UMKM menjadi sarana penyampaian ilmu dan wadah interaksi bagi para pelaku usaha untuk saling berkontribusi, menyampaikan pemikiran,
+              dan berbagi pengalaman dalam membangun serta mengembangkan jaringan usaha.
+          </td>
+        </tr>
+    </table>
+  </div>
+   
+  <!-- Event -->
+  <div class="w3-container w3-xlarge w3-padding-64">
+    <div class="w3-center w3-padding-24">
+      <img src="img/7.jpg"> <span style="padding-left: 40px;">EVENT</span>
+    </div>
+
+  <div class="w3-row" style="max-width: 1500px;">
+    <div class="w3-third w3-left">
+      <div class="container">
+        <div class="card" style="width:400px">
+          <img class="card-img-top" src="img/321.jpg" alt="Card image" style="width:100%">
+            <div class="card-body">
+              <h4 class="card-title">John Doe</h4>
+              <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+              <a href="#" class="btn btn-primary">Daftar</a>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="w3-third w3-middle">
+      <div class="container">
+        <div class="card" style="width:400px">
+          <img class="card-img-top" src="img/321.jpg" alt="Card image" style="width:100%">
+            <div class="card-body">
+              <h4 class="card-title">John Doe</h4>
+              <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+              <a href="#" class="btn btn-primary">Daftar</a>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="w3-third w3-right">
+      <div class="container">
+        <div class="card" style="width:400px">
+          <img class="card-img-top" src="img/321.jpg" alt="Card image" style="width:100%">
+            <div class="card-body">
+              <h4 class="card-title">John Doe</h4>
+              <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+              <a href="#" class="btn btn-primary">Daftar</a>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </div>
+
+   <!-- What People Say -->
+   <div class="w3-container w3-xlarge w3-padding-64">
+    <div class="w3-center w3-padding-24">
+    <span>WHAT <b>PEOPLE SAY</b></span>
+    <br>
+    <span> LOGO SUMKM </span>
+    </div>
+
+  <div class="w3-row" style="max-width: 1500px;">
+    <div class="w3-third w3-left">
+      <div class="container">
+        <div class="card" style="width:400px">
+          <img class="card-img-top" src="img/321.jpg" alt="Card image" style="width:100%">
+            <div class="card-body">
+              <h4 class="card-title">John Doe</h4>
+              <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+              <a href="#" class="btn btn-primary">Daftar</a>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="w3-third w3-middle">
+      <div class="container">
+        <div class="card" style="width:400px">
+          <img class="card-img-top" src="img/321.jpg" alt="Card image" style="width:100%">
+            <div class="card-body">
+              <h4 class="card-title">John Doe</h4>
+              <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+              <a href="#" class="btn btn-primary">Daftar</a>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="w3-third w3-right">
+      <div class="container">
+        <div class="card" style="width:400px">
+          <img class="card-img-top" src="img/321.jpg" alt="Card image" style="width:100%">
+            <div class="card-body">
+              <h4 class="card-title">John Doe</h4>
+              <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+              <a href="#" class="btn btn-primary">Daftar</a>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </div>
+
+
+</div>
+
+
+ 
+</body>
+</html>
